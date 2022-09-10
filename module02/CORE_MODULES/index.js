8f37dcf70046eca2cd78df98a54a1ec17f635f2c
Maruf Mobin <mobinh264@gmail.com>
2022-09-10 12:16:26 +0600

    What is global object and modules

---
+++ module02/CORE_MODULES/index.js
@@ -0,0 +1,6 @@
+// const name = require('./other');
+const { add, name } = require('./other');
+
+let ans = add( 4, 4 )
+console.log(name);
+console.log(ans);
---
+++ module02/CORE_MODULES/other.js
@@ -0,0 +1,9 @@
+var name = "Maruf Mobin";
+const add = ( num1, num2 ) =>{
+    return num1 + num2;
+}
+module.exports = {
+        add,
+        name
+}
+// module.exports = name; // Expecitly Export 
